834d0f8e351c42527941d72f06e5e30e63d108b4
Natalia <n.v@outlook.cl>
2017-05-21 22:59:02 -0400

    Subiendo funcion JS faltaron dos botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
+function restaurar() {
+    document.getElementById("uno-pa").setAttribute("class", "pandas");
+    document.getElementById("dos-pa").setAttribute("class", "pandas");
+    document.getElementById("tres-pa").setAttribute("class", "pandas");
+    document.getElementById("cuatro-pa").setAttribute("class", "pandas");
 
+}
+
+
+function close01() {
+    document.getElementById("uno-pa").setAttribute("class", "pandasuno-pa");
+}
+
+function close02() {
+    document.getElementById("dos-pa").setAttribute("class", "pandasdos-pa");
+}
+
+function close03() {
+    document.getElementById("tres-pa").setAttribute("class", "pandastres-pa");
+}
+
+function close04() {
+    document.getElementById("cuatro-pa").setAttribute("class", "pandascuatro-pa");
+}
